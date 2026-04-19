c6b848b02a6267d1d266fa44c7496e652f468c81
JAM9973 <alexcho6323@gmail.com>
2026-04-20 01:21:17 +0900

    아스키코드 변환
    중간고사 공부 테스트

---
+++ test1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	char ch1 = 106, ch2 = 97, ch3 = 101;
+
+	int in1 = 'm', in2 = 'i', in3 = 'n';
+
+	printf("%c%c%c\n", ch1, ch2, ch3);
+	printf("%c%c%c\n", in1, in2, in3);
+
+	return 0;
+}
+
+
+
